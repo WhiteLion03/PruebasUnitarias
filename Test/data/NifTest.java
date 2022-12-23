8a119c4ea2cc99400b3e92627e592e88cbe94b74
jordi <jordiarenasr@gmailcom>
2022-12-23 19:35:19 +0100

    Classe DigitalSignature

--- Test/data/NifTest.java
+++ Test/data/NifTest.java
@@ -9,6 +9,16 @@ class NifTest {
 
     @Test
     void testEquals() {
+        try{
+            Nif nif1 = new Nif ("40235234F");
+            Nif nif2 = new Nif ("40235234F");
+            Nif nif3 = new Nif ("40235234C");
+            assertTrue(nif1.equals(nif2));
+            assertFalse(nif1.equals(nif3));
+            assert
+        }catch (NotCorrectFormatException e){
+
+        }
     }
 
     @Test
