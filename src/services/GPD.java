9a8b3897f51f82df9f99a8fc88a2cd5fcac34a2f
jordi <jordiarenasr@gmailcom>
2022-12-31 17:58:56 +0100

    Interficie GPD

---
+++ src/services/GPD.java
@@ -0,0 +1,8 @@
+package services;
+
+import data.Goal;
+import publicadministration.Citizen;
+
+public interface GPD {// External service that represents the General Police Direction
+    boolean verifyData(Citizen persData, Goal goal) throws IncorrectVerificationException, ConnectException;
+}
